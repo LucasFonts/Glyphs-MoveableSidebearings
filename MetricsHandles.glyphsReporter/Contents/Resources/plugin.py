7d22d97b65c7bd6118bd02873d628b090681bb7a
Jens Kutilek <webmail@kutilek.de>
2020-02-05 18:13:13 +0100

    Initial import

---
+++ MetricsHandles.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,246 @@
+# encoding: utf-8
+from __future__ import division, print_function, unicode_literals
+
+import objc
+from GlyphsApp import Glyphs, MOUSEDOWN, MOUSEUP
+from GlyphsApp.plugins import *
+from GlyphsApp.plugins import ReporterPlugin, SelectTool
+from AppKit import NSAffineTransform, NSApplication, NSAlternateKeyMask, \
+    NSBezierPath, NSClassFromString, NSColor, NSCommandKeyMask, NSFont, \
+    NSFontAttributeName, NSForegroundColorAttributeName, NSMakeRect, \
+    NSMenuItem, NSMutableParagraphStyle, NSPoint, NSRect, NSShiftKeyMask, \
+    NSString
+
+
+plugin_id = "com.lucasfonts.MetricsHandles"
+DEBUG = False
+SNAP_TOLERANCE = 5
+
+
+class MetricsHandles(ReporterPlugin):
+
+    @objc.python_method
+    def settings(self):
+        self.menuName = "Metrics Handles"
+        # self.generalContextMenus = [
+        #     {
+        #         "name": Glyphs.localize({
+        #             'en': u'Show Error Labels',
+        #             'de': u'Fehlerbeschriftung anzeigen'}),
+        #         "action": self.toggleLabels
+        #     },
+        # ]
+
+    @objc.python_method
+    def start(self):
+        self.active_metric = None
+        self.drag_start = None
+        self.dragging = False
+        self.mouse_position = (0, 0)
+
+    @objc.python_method
+    def willActivate(self):
+        Glyphs.addCallback(self.mouseDown_, MOUSEDOWN)
+        Glyphs.addCallback(self.mouseUp_, MOUSEUP)
+
+    @objc.python_method
+    def willDeactivate(self):
+        Glyphs.removeCallback(self.mouseDown_, MOUSEDOWN)
+        Glyphs.removeCallback(self.mouseUp_, MOUSEUP)
+
+    @objc.python_method
+    def foreground(self, layer):
+        # Get the mouse position
+        try:
+            self.mouse_position = self.controller.graphicView().getActiveLocation_(
+                Glyphs.currentEvent()
+            )
+        except Exception as e:
+            self.logToConsole("foreground: mouse_position: %s" % str(e))
+            self.mouse_position = (0, 0)
+
+        x, y = self.mouse_position
+
+        if self.dragging:
+            metricsLocked = self.metricsAreLocked(layer)
+            if self.metricsAreLocked(layer):
+                side = self.active_metric[0]
+                if side == "LSB":
+                    x = 0.5
+                elif side == "RSB":
+                    x = layer.width - 0.5
+            self._drawHandle(
+                handle_x=(x, 1),
+                width=1 / self.getScale(),
+                metric=(
+                    self.active_metric[0],
+                    self.getDraggedDistance(),
+                    layer
+                ),
+                alpha=1.0,
+                locked=metricsLocked
+            )
+            return
+
+        # Is the mouse inside the "hot spot"?
+        # If yes, display drag handles
+
+        master = layer.master
+        if y < master.descender or y > master.ascender:
+            self.active_metric = None
+            return
+
+        if x < 0 or x > layer.width:
+            # Mouse is outside the glyph
+            self.active_metric = None
+            return
+
+        snap_tolerance = SNAP_TOLERANCE / self.getScale()
+
+        if x > snap_tolerance and x < layer.width - snap_tolerance:
+            # Mouse is too far inside the glyph
+            self.active_metric = None
+            return
+
+        currentController = self.controller.view().window().windowController()
+        if currentController:
+            tool = currentController.toolDrawDelegate()
+            # don't activate if on cursor tool, or pan tool
+            if not(
+                # tool.isKindOfClass_(NSClassFromString("GlyphsToolText")) or
+                tool.isKindOfClass_(NSClassFromString("GlyphsToolHand")) or
+                tool.isKindOfClass_(NSClassFromString("GlyphsToolTrueTypeInstructor"))
+            ):
+                if x < snap_tolerance:
+                    handle_x = (0, snap_tolerance)
+                    metric = ("LSB", layer.LSB, layer)
+                else:
+                    handle_x = (layer.width - snap_tolerance, snap_tolerance)
+                    metric = ("RSB", layer.RSB, layer)
+                self.active_metric = metric
+                self._drawHandle(handle_x, snap_tolerance, metric)
+        else:
+            self.active_metric = None
+
+    @objc.python_method
+    def _drawHandle(self, handle_x, width, metric, alpha=0.3, locked=False):
+        pos, width = handle_x
+        master = metric[2].master
+        NSColor.colorWithCalibratedRed_green_blue_alpha_(
+            0.9,
+            0.1,
+            0.0,
+            alpha
+        ).set()
+        rect = NSRect(
+            origin=(pos, master.descender),
+            size=(width, master.ascender - master.descender)
+        )
+        NSBezierPath.bezierPathWithRect_(rect).fill()
+        self._drawTextLabel(handle_x, width, metric, alpha, locked)
+
+    @objc.python_method
+    def _drawTextLabel(self, handle_x, width, metric, alpha=0.3, locked=False):
+        scale = self.getScale()
+        text_size = 11 / scale
+        text_dist = 5 / scale
+        metric_name, value, layer = metric
+        if locked:
+            shown_value = "🔒︎"
+        elif metric_name == "LSB" and self.dragging:
+            shown_value = "%g ∆%g" % (layer.LSB - value, value)
+        elif metric_name == "RSB" and self.dragging:
+            shown_value = "%g ∆%g" % (layer.RSB + value, value)
+        else:
+            shown_value = "%g" % value
+
+        attrs = {
+            NSFontAttributeName: NSFont.systemFontOfSize_(text_size),
+            NSForegroundColorAttributeName: NSColor.colorWithCalibratedRed_green_blue_alpha_(
+                0.9,
+                0.1,
+                0.0,
+                alpha
+            ),
+        }
+        myString = NSString.string().stringByAppendingString_(shown_value)
+        bbox = myString.sizeWithAttributes_(attrs)
+        bw = bbox.width
+        bh = bbox.height
+
+        text_pt = NSPoint()
+        text_pt.y = self.mouse_position[1]
+        if metric_name == "LSB":
+            if self.dragging:
+                text_pt.x = handle_x[0] + text_dist
+            else:
+                text_pt.x = width + text_dist
+        elif metric_name == "RSB":
+            if self.dragging:
+                text_pt.x = handle_x[0] - text_dist - bw
+            else:
+                text_pt.x = layer.width - width - text_dist - bw
+        else:
+            text_pt.x = self.mouse_position[0] - text_dist - bw
+
+        rr = NSRect(
+            origin=(text_pt.x, text_pt.y),
+            size=(bw, bh)
+        )
+        myString.drawInRect_withAttributes_(rr, attrs)
+
+    @objc.python_method
+    def getDraggedDistance(self):
+        if self.drag_start is None:
+            return 0
+
+        return int(round(self.mouse_position[0] - self.drag_start))
+
+    @objc.python_method
+    def metricsAreLocked(self, layer):
+        cp = "Link Metrics With First Master"
+        if cp in layer.master.customParameters and layer.master.customParameters[cp] == 1:
+            return True
+        return False
+
+    @objc.python_method
+    def setMetrics(self, metrics, delta):
+        if delta == 0:
+            return
+
+        side, _, layer = metrics
+
+        if self.metricsAreLocked(layer):
+            return
+
+        layer.parent.beginUndo()
+        if side == "LSB":
+            layer.LSB -= delta
+        elif side == "RSB":
+            layer.width += delta
+        else:
+            layer.LSB += delta
+            layer.width -= delta
+        layer.parent.endUndo()
+
+    def mouseDown_(self, event):
+        # if event.clickCount() == 3:
+        #     self.mouseTripleDown_(event)
+        #     return
+        # if event.clickCount() == 2:
+        #     self.mouseDoubleDown_(event)
+        #     return
+        if self.drag_start is None and self.active_metric is not None:
+            self.dragging = True
+            self.drag_start = self.mouse_position[0]
+
+    def mouseUp_(self, event):
+        self.dragging = False
+        # Set changed metric
+        if self.drag_start is None:
+            return
+
+        dist = self.getDraggedDistance()
+        self.setMetrics(self.active_metric, dist)
+
+        self.drag_start = None
