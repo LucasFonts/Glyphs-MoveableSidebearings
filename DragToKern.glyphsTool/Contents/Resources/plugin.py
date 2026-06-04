c62ee6359f8c6370dbda71367125c63bad70ac43
Jens Kutilek <webmail@kutilek.de>
2026-06-04 14:15:42 +0200

    Autoformat

--- DragToKern.glyphsTool/Contents/Resources/plugin.py
+++ DragToKern.glyphsTool/Contents/Resources/plugin.py
@@ -1,9 +1,6 @@
-from __future__ import annotations
-
 from typing import Any
 
 import objc
-from Foundation import NSClassFromString, NSPoint, NSRect, NSString
 from AppKit import (
     NSBezierPath,
     NSColor,
@@ -14,6 +11,7 @@ from AppKit import (
     NSForegroundColorAttributeName,
     NSGradient,
 )
+from Foundation import NSClassFromString, NSPoint, NSRect, NSString
 from GlyphsApp import GSLTR, MOUSEMOVED, Glyphs
 from GlyphsApp.plugins import SelectTool
 
@@ -90,10 +88,12 @@ def handleException(composedLayers, layerIndex, c, direction=GSLTR) -> None:
 class DragToKern(SelectTool):
     @objc.python_method
     def settings(self) -> None:
-        self.name = Glyphs.localize({
-            "en": "Mouse Kerning and Spacing",
-            "de": "Unterschneidung und Zurichtung per Maus",
-        })
+        self.name = Glyphs.localize(
+            {
+                "en": "Mouse Kerning and Spacing",
+                "de": "Unterschneidung und Zurichtung per Maus",
+            }
+        )
         self.keyboardShortcut = "k"
         self.stdCursor = NSCursor.resizeLeftRightCursor()
         self.lckCursor = NSCursor.operationNotAllowedCursor()
@@ -145,18 +145,26 @@ class DragToKern(SelectTool):
     @objc.python_method
     def conditionalContextMenus(self) -> list[dict[str, Any]]:
         if self.drawMeasurements:
-            return [{
-                "name": Glyphs.localize({
-                    "en": "Hide Measurements While Spacing",
-                }),
+            return [
+                {
+                    "name": Glyphs.localize(
+                        {
+                            "en": "Hide Measurements While Spacing",
+                        }
+                    ),
+                    "action": self.toggleMeasurements_,
+                }
+            ]
+        return [
+            {
+                "name": Glyphs.localize(
+                    {
+                        "en": "Show Measurements While Spacing",
+                    }
+                ),
                 "action": self.toggleMeasurements_,
-            }]
-        return [{
-            "name": Glyphs.localize({
-                "en": "Show Measurements While Spacing",
-            }),
-            "action": self.toggleMeasurements_,
-        }]
+            }
+        ]
 
     def toggleMeasurements_(self, sender=None) -> None:
         self.drawMeasurements = not self.drawMeasurements
